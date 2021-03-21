ee2e3da9dc7f0281588efd9ab37b373d83ba0148
dscottraynsford <dscottraynsford@outlook.com>
2021-03-21 16:42:18 +1300

    AB#829: Added configuration object

---
+++ TailwindTraders/TailwindTraders.WebApp/Classes/WebAppConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TailwindTraders.WebApp.Classes
+{
+    public static class WebAppConfig
+    {
+        public static string AppName { get; }
+
+        static WebAppConfig() {
+            AppName = "Tailwind Traders v2.0";
+        }
+    }
+}
