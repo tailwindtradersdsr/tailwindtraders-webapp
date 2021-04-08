1320a66cfa796c53146d2ab17dffce63ac2e3989
dscottraynsford <dscottraynsford@outlook.com>
2021-04-08 15:10:40 +1200

    AB#851: Add application insights.

--- TailwindTraders/TailwindTraders.WebApp/Startup.cs
+++ TailwindTraders/TailwindTraders.WebApp/Startup.cs
@@ -24,6 +24,7 @@ namespace TailwindTraders.WebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
